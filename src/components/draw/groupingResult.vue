af154962e59668be49df9fc155b6debda8636059
Alcantara <yjing_sjtu@163.com>
2018-03-04 23:59:07 +0800

    css3方法transform: translateY实现当前小组slidedown高亮效果,层叠上下文和z-index的运用

--- src/components/draw/groupingResult.vue
+++ src/components/draw/groupingResult.vue
@@ -3,25 +3,30 @@
     http://localhost:8000/draw
     component: grouping-result
 -->
-<!-- 如何使用非响应式数据？定义局部变量，使其等于data（props）中的数据，例如需要分组落位的球队 -->
-<!-- 3.2 优化样式 -->
+<!-- 3.2 优化样式 
+     当前落位的小组高亮，层叠上下文和z-index的运用
+-->
 
 <template>
     <div id="grouping-result">
         <div class='logo'>
-            <img src="../../assets/Worldcup logo.jpg" alt="俄罗斯世界杯抽签仪式">
+            <img src="../../assets/WorldCupLogo.jpg" alt="俄罗斯世界杯抽签仪式">
         </div>
         <div class="main">
             <h2>分组结果</h2>
             <!-- 每一档的球队 -->
-            <div :class="['group', {'current-group': gIndex === curGroupNum - 1}]" v-for="(group,gIndex) in groupContainer">
+            <div class="group" v-for="(group,gIndex) in groupContainer">
                 <h3>{{ group[0].groupChar }}</h3>
-                <div class="group-box">
+                <div class="group-teams">    
                     <ul>
-                        <li class="group-idx" v-for="pos in group">
+                        <li class="group-idx" v-for="pos in group" :key="pos.id">
+                            <img v-if="pos.team" :src="pos.team.flagUrl">
                             <span v-if="pos.team">{{ pos.team.teamName }}</span>
                         </li>
                     </ul>
+                    <transition name="frame">
+                        <div class="frame" v-if="gIndex === curGroupNum - 1"></div>
+                    </transition>
                 </div>
             </div>
         </div>
@@ -44,7 +49,7 @@ export default {
     text-align: center;
 }
 .logo {
-    flex: 0 0 15%;
+    flex: 0 0 200px;
     border-right: 3px solid #ede275;
     box-sizing: border-box;  /*图片宽度与父容器相等*/
 }
@@ -78,14 +83,42 @@ export default {
 .group h3 {
     background-color: #e55451;
 }
-.current-group {
-    background-color: #e55451;
+.group-teams {
+    position: relative;
 }
-
 /*每个位次*/
 .group-idx {
     height: 20px;
-    margin-bottom: 2px;
+    padding: 3px 0 3px 5px;
     border-bottom: 1px dashed #ccc;
+    /*每个位次球队显示需要设置positon：relative和index最上，用于高亮scale()过渡*/
+    position: relative;
+    z-index: 99;
+    text-align: left;
+}
+.group-idx img {
+    height: 100%;
+}
+
+/*当前落位的小组高亮*/
+.frame {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #ffebcd;
+    transform-origin: center 0;
+    z-index: 0;
+}
+.frame-enter {
+    transform: scaleY(0);
+}
+.frame-leave-to {
+    opacity: 0;
+}
+.frame-enter-active,
+.frame-leave-active {
+    transition: all .5s;
 }
 </style>
